7f6d3d0c5fb9ec98fca7295d0c1b55f024dbeff0
Marie-Elyse Lafaille-Magnan <marieelyse.lafaillemagnan@gmail.com>
2020-04-24 13:20:12 -0400

    Add files via upload

---
+++ Low_density_SNPs_generation.py
@@ -0,0 +1,569 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Marie-Elyse Lafaille-Magnan APR 2020
+
+# code to impute missing information in your typical cohort data
+# test to impute missing snps data or other tabular data with ctgan and compare to other imputation methods 
+# to do try to implement other imputation method and examine csv file 
+
+import torch
+from torch import nn
+from torch.autograd import Variable
+
+from torch import optim
+from torch.nn import functional
+
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.mixture import BayesianGaussianMixture
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.utils.testing import ignore_warnings
+# from sklearn.utils._testing import ignore_warnings
+
+from ctgan.conditional import ConditionalGenerator
+from ctgan.models import Discriminator, Generator
+from ctgan.sampler import Sampler
+# from ctgan.transformer import DataTransformer
+
+import fancyimpute
+from fancyimpute import IterativeImputer 
+from fancyimpute import KNN
+from fancyimpute import SoftImpute
+from fancyimpute import BiScaler
+from fancyimpute import IterativeSVD
+from fancyimpute import NuclearNormMinimization
+from fancyimpute import IterativeSVD
+# SoftImpute AND IterativeSVD, SimpleFill, MatrixFactorization, and 
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import sklearn.datasets as SKD
+
+
+data = pd.read_csv(
+    'ai_mavan_adhd7.csv', 
+    sep=',', 
+    index_col=None
+)
+
+
+# In[ ]:
+
+
+# MICE IMPUTATION
+mice_impute = IterativeImputer()
+traindatafill = mice_impute.fit_transform(adhd)
+
+
+# In[ ]:
+
+
+# KNN way to impute
+
+adhd_filled_knn = KNN(k=3).fit_transform(adhd) #use 3 nearest rows which have a feature to fill in each row’s missing features
+
+
+# In[ ]:
+
+
+# NUCLEARNOMMINIMIZATION
+adhd_filled_nnm = NuclearNormMinimization().fit_transform(adhd)
+
+
+# In[69]:
+
+
+#ENTER COLUMNS LABELS THAT HAVE DISCRETE VARIABLES
+
+discrete_columns = [
+    'Hamilton',
+    'gender_male',
+ 'Dob_MONTH_DIGIT',
+ 'Hamilton',
+ 'above_college',
+ 'QuintMat_w',
+ 'QuintSoc_w',
+ 'Mood_drug',
+ 'Pren_income4',
+ 'No_depression',
+ 'Postpartum_depression',
+ 'Materl_anxiety',
+ 'B_HTTLPR_2',
+ 'B_DRD1_hap',
+ 'B_OXT_pep1',
+ 'B_TPH2',
+ 'B_HTR1A',
+ 'B_HTR1B_best',
+ 'B_HTR2A_alt_r',
+ 'B_DRD4',
+ 'B_DRD4_78',
+ 'B_DAT',
+ 'B_DRD2',
+ 'B_DRD2_rs1799978',
+ 'B_DRD3_rs6280',
+ 'B_GR_rs10052957',
+ 'B_COMT',
+ 'B_COMT_cat',
+ 'B_COMT_rs165599',
+ 'B_BDNF_r'
+]
+
+
+# In[70]:
+
+
+#CTGANSynthesizer was copied from github
+
+class CTGANSynthesizer(object):
+    """Conditional Table GAN Synthesizer.
+    This is the core class of the CTGAN project, where the different components
+    are orchestrated together.
+    For more details about the process, please check the [Modeling Tabular data using
+    Conditional GAN](https://arxiv.org/abs/1907.00503) paper.
+    Args:
+        embedding_dim (int):
+            Size of the random sample passed to the Generator. Defaults to 128.
+        gen_dim (tuple or list of ints):
+            Size of the output samples for each one of the Residuals. A Resiudal Layer
+            will be created for each one of the values provided. Defaults to (256, 256).
+        dis_dim (tuple or list of ints):
+            Size of the output samples for each one of the Discriminator Layers. A Linear Layer
+            will be created for each one of the values provided. Defaults to (256, 256).
+        l2scale (float):
+            Wheight Decay for the Adam Optimizer. Defaults to 1e-6.
+        batch_size (int):
+            Number of data samples to process in each step.
+    """
+
+    def __init__(self, embedding_dim=128, gen_dim=(256, 256), dis_dim=(256, 256),
+                 l2scale=1e-6, batch_size=500):
+
+        self.embedding_dim = embedding_dim
+        self.gen_dim = gen_dim
+        self.dis_dim = dis_dim
+
+        self.l2scale = l2scale
+        self.batch_size = batch_size
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    def _apply_activate(self, data):
+        data_t = []
+        st = 0
+        for item in self.transformer.output_info:
+            if item[1] == 'tanh':
+                ed = st + item[0]
+                data_t.append(torch.tanh(data[:, st:ed]))
+                st = ed
+            elif item[1] == 'softmax':
+                ed = st + item[0]
+                data_t.append(functional.gumbel_softmax(data[:, st:ed], tau=0.2))
+                st = ed
+            else:
+                assert 0
+
+        return torch.cat(data_t, dim=1)
+
+    def _cond_loss(self, data, c, m):
+        loss = []
+        st = 0
+        st_c = 0
+        skip = False
+        for item in self.transformer.output_info:
+            if item[1] == 'tanh':
+                st += item[0]
+                skip = True
+
+            elif item[1] == 'softmax':
+                if skip:
+                    skip = False
+                    st += item[0]
+                    continue
+
+                ed = st + item[0]
+                ed_c = st_c + item[0]
+                tmp = functional.cross_entropy(
+                    data[:, st:ed],
+                    torch.argmax(c[:, st_c:ed_c], dim=1),
+                    reduction='none'
+                )
+                loss.append(tmp)
+                st = ed
+                st_c = ed_c
+
+            else:
+                assert 0
+
+        loss = torch.stack(loss, dim=1)
+
+        return (loss * m).sum() / data.size()[0]
+
+    def fit(self, train_data, discrete_columns=tuple(), epochs=300, log_frequency=True):
+        """Fit the CTGAN Synthesizer models to the training data.
+        Args:
+            train_data (numpy.ndarray or pandas.DataFrame):
+                Training Data. It must be a 2-dimensional numpy array or a
+                pandas.DataFrame.
+            discrete_columns (list-like):
+                List of discrete columns to be used to generate the Conditional
+                Vector. If ``train_data`` is a Numpy array, this list should
+                contain the integer indices of the columns. Otherwise, if it is
+                a ``pandas.DataFrame``, this list should contain the column names.
+            epochs (int):
+                Number of training epochs. Defaults to 300.
+            log_frequency (boolean):
+                Whether to use log frequency of categorical levels in conditional
+                sampling. Defaults to ``True``.
+        """
+
+        self.transformer = DataTransformer()
+        self.transformer.fit(train_data, discrete_columns)
+        train_data = self.transformer.transform(train_data)
+
+        data_sampler = Sampler(train_data, self.transformer.output_info)
+
+        data_dim = self.transformer.output_dimensions
+        self.cond_generator = ConditionalGenerator(
+            train_data,
+            self.transformer.output_info,
+            log_frequency
+        )
+
+        self.generator = Generator(
+            self.embedding_dim + self.cond_generator.n_opt,
+            self.gen_dim,
+            data_dim
+        ).to(self.device)
+
+        discriminator = Discriminator(
+            data_dim + self.cond_generator.n_opt,
+            self.dis_dim
+        ).to(self.device)
+
+        optimizerG = optim.Adam(
+            self.generator.parameters(), lr=2e-4, betas=(0.5, 0.9),
+            weight_decay=self.l2scale
+        )
+        optimizerD = optim.Adam(discriminator.parameters(), lr=2e-4, betas=(0.5, 0.9))
+
+        assert self.batch_size % 2 == 0
+        mean = torch.zeros(self.batch_size, self.embedding_dim, device=self.device)
+        std = mean + 1
+
+        steps_per_epoch = max(len(train_data) // self.batch_size, 1)
+        for i in range(epochs):
+            for id_ in range(steps_per_epoch):
+                fakez = torch.normal(mean=mean, std=std)
+
+                condvec = self.cond_generator.sample(self.batch_size)
+                if condvec is None:
+                    c1, m1, col, opt = None, None, None, None
+                    real = data_sampler.sample(self.batch_size, col, opt)
+                else:
+                    c1, m1, col, opt = condvec
+                    c1 = torch.from_numpy(c1).to(self.device)
+                    m1 = torch.from_numpy(m1).to(self.device)
+                    fakez = torch.cat([fakez, c1], dim=1)
+
+                    perm = np.arange(self.batch_size)
+                    np.random.shuffle(perm)
+                    real = data_sampler.sample(self.batch_size, col[perm], opt[perm])
+                    c2 = c1[perm]
+
+                fake = self.generator(fakez)
+                fakeact = self._apply_activate(fake)
+
+                real = torch.from_numpy(real.astype('float32')).to(self.device)
+
+                if c1 is not None:
+                    fake_cat = torch.cat([fakeact, c1], dim=1)
+                    real_cat = torch.cat([real, c2], dim=1)
+                else:
+                    real_cat = real
+                    fake_cat = fake
+
+                y_fake = discriminator(fake_cat)
+                y_real = discriminator(real_cat)
+
+                pen = discriminator.calc_gradient_penalty(real_cat, fake_cat, self.device)
+                loss_d = -(torch.mean(y_real) - torch.mean(y_fake))
+
+                optimizerD.zero_grad()
+                pen.backward(retain_graph=True)
+                loss_d.backward()
+                optimizerD.step()
+
+                fakez = torch.normal(mean=mean, std=std)
+                condvec = self.cond_generator.sample(self.batch_size)
+
+                if condvec is None:
+                    c1, m1, col, opt = None, None, None, None
+                else:
+                    c1, m1, col, opt = condvec
+                    c1 = torch.from_numpy(c1).to(self.device)
+                    m1 = torch.from_numpy(m1).to(self.device)
+                    fakez = torch.cat([fakez, c1], dim=1)
+
+                fake = self.generator(fakez)
+                fakeact = self._apply_activate(fake)
+
+                if c1 is not None:
+                    y_fake = discriminator(torch.cat([fakeact, c1], dim=1))
+                else:
+                    y_fake = discriminator(fakeact)
+
+                if condvec is None:
+                    cross_entropy = 0
+                else:
+                    cross_entropy = self._cond_loss(fake, c1, m1)
+
+                loss_g = -torch.mean(y_fake) + cross_entropy
+
+                optimizerG.zero_grad()
+                loss_g.backward()
+                optimizerG.step()
+
+            print("Epoch %d, Loss G: %.4f, Loss D: %.4f" %
+                  (i + 1, loss_g.detach().cpu(), loss_d.detach().cpu()),
+                  flush=True)
+
+    def sample(self, n):
+        """Sample data similar to the training data.
+        Args:
+            n (int):
+                Number of rows to sample.
+        Returns:
+            numpy.ndarray or pandas.DataFrame
+        """
+
+        steps = n // self.batch_size + 1
+        data = []
+        for i in range(steps):
+            mean = torch.zeros(self.batch_size, self.embedding_dim)
+            std = mean + 1
+            fakez = torch.normal(mean=mean, std=std).to(self.device)
+
+            condvec = self.cond_generator.sample_zero(self.batch_size)
+            if condvec is None:
+                pass
+            else:
+                c1 = condvec
+                c1 = torch.from_numpy(c1).to(self.device)
+                fakez = torch.cat([fakez, c1], dim=1)
+
+            fake = self.generator(fakez)
+            fakeact = self._apply_activate(fake)
+            data.append(fakeact.detach().cpu().numpy())
+
+        data = np.concatenate(data, axis=0)
+        data = data[:n]
+
+        return self.transformer.inverse_transform(data, None)
+
+
+# In[71]:
+
+
+# DataTransformer was copied from github
+class DataTransformer(object):
+    """DataTransformer.
+    Model continuous columns with a BayesianGMM and normalized to a scalar
+    [0, 1] and a vector.
+    Discrete columns are encoded using a scikit-learn OneHotEncoder.
+    Args:
+        n_cluster (int):
+            Number of modes.
+        epsilon (float):
+            Epsilon value.
+    """
+
+    def __init__(self, n_clusters=10, epsilon=0.005):
+        self.n_clusters = n_clusters
+        self.epsilon = epsilon
+
+    @ignore_warnings(category=ConvergenceWarning)
+    def _fit_continuous(self, column, data):
+        gm = BayesianGaussianMixture(
+            self.n_clusters,
+            weight_concentration_prior_type='dirichlet_process',
+            weight_concentration_prior=0.001,
+            n_init=1
+        )
+        gm.fit(data)
+        components = gm.weights_ > self.epsilon
+        num_components = components.sum()
+
+        return {
+            'name': column,
+            'model': gm,
+            'components': components,
+            'output_info': [(1, 'tanh'), (num_components, 'softmax')],
+            'output_dimensions': 1 + num_components,
+        }
+
+    def _fit_discrete(self, column, data):
+        ohe = OneHotEncoder(sparse=False)
+        ohe.fit(data)
+        categories = len(ohe.categories_[0])
+
+        return {
+            'name': column,
+            'encoder': ohe,
+            'output_info': [(categories, 'softmax')],
+            'output_dimensions': categories
+        }
+
+    def fit(self, data, discrete_columns=tuple()):
+        self.output_info = []
+        self.output_dimensions = 0
+
+        if not isinstance(data, pd.DataFrame):
+            self.dataframe = False
+            data = pd.DataFrame(data)
+        else:
+            self.dataframe = True
+
+        self.dtypes = data.infer_objects().dtypes
+        self.meta = []
+        for column in data.columns:
+            column_data = data[[column]].values
+            if column in discrete_columns:
+                meta = self._fit_discrete(column, column_data)
+            else:
+                meta = self._fit_continuous(column, column_data)
+
+            self.output_info += meta['output_info']
+            self.output_dimensions += meta['output_dimensions']
+            self.meta.append(meta)
+
+    def _transform_continuous(self, column_meta, data):
+        components = column_meta['components']
+        model = column_meta['model']
+
+        means = model.means_.reshape((1, self.n_clusters))
+        stds = np.sqrt(model.covariances_).reshape((1, self.n_clusters))
+        features = (data - means) / (4 * stds)
+
+        probs = model.predict_proba(data)
+
+        n_opts = components.sum()
+        features = features[:, components]
+        probs = probs[:, components]
+
+        opt_sel = np.zeros(len(data), dtype='int')
+        for i in range(len(data)):
+            pp = probs[i] + 1e-6
+            pp = pp / pp.sum()
+            opt_sel[i] = np.random.choice(np.arange(n_opts), p=pp)
+
+        idx = np.arange((len(features)))
+        features = features[idx, opt_sel].reshape([-1, 1])
+        features = np.clip(features, -.99, .99)
+
+        probs_onehot = np.zeros_like(probs)
+        probs_onehot[np.arange(len(probs)), opt_sel] = 1
+        return [features, probs_onehot]
+
+    def _transform_discrete(self, column_meta, data):
+        encoder = column_meta['encoder']
+        return encoder.transform(data)
+
+    def transform(self, data):
+        if not isinstance(data, pd.DataFrame):
+            data = pd.DataFrame(data)
+
+        values = []
+        for meta in self.meta:
+            column_data = data[[meta['name']]].values
+            if 'model' in meta:
+                values += self._transform_continuous(meta, column_data)
+            else:
+                values.append(self._transform_discrete(meta, column_data))
+
+        return np.concatenate(values, axis=1).astype(float)
+
+    def _inverse_transform_continuous(self, meta, data, sigma):
+        model = meta['model']
+        components = meta['components']
+
+        u = data[:, 0]
+        v = data[:, 1:]
+
+        if sigma is not None:
+            u = np.random.normal(u, sigma)
+
+        u = np.clip(u, -1, 1)
+        v_t = np.ones((len(data), self.n_clusters)) * -100
+        v_t[:, components] = v
+        v = v_t
+        means = model.means_.reshape([-1])
+        stds = np.sqrt(model.covariances_).reshape([-1])
+        p_argmax = np.argmax(v, axis=1)
+        std_t = stds[p_argmax]
+        mean_t = means[p_argmax]
+        column = u * 4 * std_t + mean_t
+
+        return column
+
+    def _inverse_transform_discrete(self, meta, data):
+        encoder = meta['encoder']
+        return encoder.inverse_transform(data)
+
+    def inverse_transform(self, data, sigmas):
+        start = 0
+        output = []
+        column_names = []
+        for meta in self.meta:
+            dimensions = meta['output_dimensions']
+            columns_data = data[:, start:start + dimensions]
+
+            if 'model' in meta:
+                sigma = sigmas[start] if sigmas else None
+                inverted = self._inverse_transform_continuous(meta, columns_data, sigma)
+            else:
+                inverted = self._inverse_transform_discrete(meta, columns_data)
+
+            output.append(inverted)
+            column_names.append(meta['name'])
+            start += dimensions
+
+        output = np.column_stack(output)
+        output = pd.DataFrame(output, columns=column_names).astype(self.dtypes)
+        if not self.dataframe:
+            output = output.values
+
+        return output
+
+
+# In[79]:
+
+
+ctgan = CTGANSynthesizer()
+ctgan.fit(data, discrete_columns)
+#need to check excel files as there are some nan causing errors 
+#Input contains NaN, infinity or a value too large for dtype('float64').
+
+
+# In[78]:
+
+
+ctgan.fit(data, discrete_columns, epochs=5)
+
+sample = ctgan.sample()
+
+samples = sample(1000)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
